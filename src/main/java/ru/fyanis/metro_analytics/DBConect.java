85ff8755f5d0a5b4994cdc2d3ec1d3a749d75949
f-yanis <f.yanis48@gmail.com>
2017-06-29 22:03:38 +0300

    готово

---
+++ src/main/java/ru/fyanis/metro_analytics/DBConect.java
@@ -0,0 +1,35 @@
+package ru.fyanis.metro_analytics;
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConect {
+    private final String URL;
+    private final String USERNAME;
+    private final String PASSWORD;
+
+    Connection connection;
+
+    public DBConect(String URL, String USERNAME, String PASSWORD) {
+        this.URL = URL;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
+
+
